a88b66f8452a9505b34fd98ddf4fc80e0c1f0a64
ibrahimohid <ibrahimohid77@gmail.com>
2024-09-20 09:47:12 +0600

    ui design

---
+++ script.js
@@ -0,0 +1,8 @@
+const filterEle = document.querySelector("#filter");
+const productNameEle = document.querySelector("#productName");
+const productPriceEle = document.querySelector("#productPrice");
+const formEle = document.querySelector("form");
+const msgEle = document.querySelector("#msg");
+const addProductsEle = document.querySelector("#addProducts");
+
+
