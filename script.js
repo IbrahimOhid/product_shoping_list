a3465339d9bff5a76d8c5e6442ba6ceb4ba39b18
ibrahimohid <ibrahimohid77@gmail.com>
2024-09-23 21:15:25 +0600

    showproduct ui

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ const nameInputElm = document.querySelector(".nameInput");
 const priceInputElm = document.querySelector(".priceInput");
 const msgElm = document.querySelector(".msg");
 const form = document.querySelector("form");
+const collectionElm =  document.querySelector('.collection');
+
+// data store memory
+let products = [];
+
+// add product data store
+function addProduct(name, price){
+  const product = {
+    id : products.length + 1,
+    name,
+    price
+  }
+  products.push(product);
+  return product;
+}
+
 
 // receive input
 function receiveInputs() {
@@ -15,8 +31,8 @@ function clearMsg(){
   msgElm.textContent = '';
 }
 // showMessage
-function showMessage(msg) {
-  const textMsg = `<div class="alert alert-danger text-center" role="alert">${msg}</div>`;
+function showMessage(msg, action = 'success') {
+  const textMsg = `<div class="alert alert-${action} text-center" role="alert">${msg}</div>`;
   msgElm.insertAdjacentHTML('afterbegin', textMsg);
   setTimeout(()=>{
     clearMsg();
@@ -27,11 +43,11 @@ function validationInputs(name, price) {
   let isValid = true;
   if (name === "" || price === "") {
     isValid = false;
-    showMessage("Please Provide Necessary Input");
+    showMessage("Please Provide Necessary Input", 'danger');
   }
   if(Number(price) !== Number(price)){
     isValid = false;
-    showMessage('Please Provide Price in Number');
+    showMessage('Please Provide Price in Number', 'danger');
   }
   return isValid;
 }
@@ -39,6 +55,23 @@ function validationInputs(name, price) {
 function resetInputs(){
   nameInputElm.value = '';
   priceInputElm.value = '';
+}
+// show product ui
+function showProductUi(productInfo){
+  const {id, name, price} = productInfo;
+  const elm = `<li
+              class="list-group-item collection-item d-flex flex-row justify-content-between mb-2" data-productId = ${id}
+            >
+              <div class="product-info">
+                <strong>${name}</strong>- <span class="price">$${price}</span>
+              </div>
+              <div class="action-btn">
+                <i style='cursor: pointer' class="bi bi-pencil-square me-2 text-success"></i>
+                <i style='cursor: pointer' class="bi bi-trash3 text-danger"></i>
+              </div>
+            </li>`
+            collectionElm.insertAdjacentHTML('afterbegin', elm);
+            showMessage('Product Added Successfully');
 }
 
 // handel submit form
@@ -52,6 +85,8 @@ function handelSubmitForm(e) {
   if(!isValid) return;
   // resetInput
   resetInputs();
+ const product = addProduct(name, price);
+ showProductUi(product);
   console.log(name, price);
 }
 
